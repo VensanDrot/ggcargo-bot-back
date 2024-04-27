776f39085ded6674adf8035a6200db0457da024c
abd-Kabir <kabirumarov@gmail.com>
2024-04-27 20:21:50 +0500

    remove unused imports

--- config/core/backends.py
+++ config/core/backends.py
@@ -1,6 +1,6 @@
 from django.contrib.auth import get_user_model
 from django.contrib.auth.backends import ModelBackend
-from django.db.models import Q, F, Sum, ExpressionWrapper, CharField
+from django.db.models import Q
 
 UserModel = get_user_model()
 
