7f39a2d54859142004717688e71299bbc7301474
abd-Kabir <kabirumarov@gmail.com>
2024-05-18 11:13:26 +0500

    overwrite datetimefield

--- config/models.py
+++ config/models.py
@@ -1,10 +1,18 @@
 from django.db import models
+from django.utils.timezone import localtime
+
+
+class LocalizedDateTimeField(models.DateTimeField):
+    def to_python(self, value):
+        value = super().to_python(value)
+        if value is not None:
+            value = localtime(value)
+        return value
 
 
 class BaseModel(models.Model):
-    created_at = models.DateTimeField(auto_now_add=True, null=True)
-    updated_at = models.DateTimeField(auto_now=True, null=True)
+    created_at = LocalizedDateTimeField(auto_now_add=True, null=True)
+    updated_at = LocalizedDateTimeField(auto_now=True, null=True)
 
     class Meta:
         abstract = True
-        # timezone = 'Asia/Tashkent'
