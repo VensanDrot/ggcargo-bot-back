28ff84093142ebf51dd9b07a236e8055af058d3a
abd-Kabir <kabirumarov@gmail.com>
2024-06-27 00:03:28 +0500

    change: orderemu uuid > id

--- apps/integrations/models.py
+++ apps/integrations/models.py
@@ -8,18 +8,17 @@ from config.models import BaseModel
 
 
 class OrderEMU(BaseModel):
-#     # id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-#     phone_number = models.CharField(max_length=30, null=True, blank=True)
-#     address = models.TextField(null=True, blank=True)
-#     town = models.CharField(max_length=55, null=True, blank=True)
-#     service = models.IntegerField(choices=EMU_SERVICE_CHOICE, null=True, blank=True)
-#
-#     tracking_link = models.URLField(null=True, blank=True)
-#
-#     customer = models.ForeignKey(Customer, on_delete=models.CASCADE, null=True, blank=True)
-#
+    # id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    phone_number = models.CharField(max_length=30, null=True, blank=True)
+    address = models.TextField(null=True, blank=True)
+    town = models.CharField(max_length=55, null=True, blank=True)
+    service = models.IntegerField(choices=EMU_SERVICE_CHOICE, null=True, blank=True)
+
+    tracking_link = models.URLField(null=True, blank=True)
+
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE, null=True, blank=True)
+
     class Meta:
-        abstract = True
         db_table = 'OrderEMU'
 
 
