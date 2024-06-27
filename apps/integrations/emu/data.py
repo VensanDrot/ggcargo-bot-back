c6f595f02e6154454b95a10b54011d8dde1fdd04
abd-Kabir <kabirumarov@gmail.com>
2024-06-27 14:23:34 +0500

    emu integration

--- apps/integrations/emu/data.py
+++ apps/integrations/emu/data.py
@@ -24,29 +24,34 @@ def emu_order(order_id, customer_full_name, order_instance: OrderEMU):
     emu_creds = emu_integration['credentials']
     emu_extra = emu_integration['extra']
 
+    items = []
+    newline = '\n                '
+    for item in order_instance.load.products.all():
+        items.append(f'<item length="0" height="0" width="0" quantity="1" mass="0" retprice="0" barcode="{item.barcode}">Посылка Express cargo</item>')
+
     xml = f"""
     <?xml version="1.0" encoding="UTF-8"?>
     <neworder newfolder="NO">
         <auth extra="{emu_extra}" login="{emu_creds['username']}" pass="{emu_creds['password']}"></auth>
         <order orderno="{order_id}">
             <barcode>{order_id}</barcode>
+            <sender>
+                <company>Express Cargo</company>
+                <phone>+998 98 363 37 67</phone>
+                <town>814303</town>
+                <address>Arnasay 7A</address>
+            </sender>
+            <receiver>
+                <person>{customer_full_name}</person>
+                <phone>{order_instance.phone_number}</phone>
+                <town>{order_instance.town}</town>
+                <address>{order_instance.address}</address>
+            </receiver>
+            <service>{order_instance.service}</service>
+            <items>
+                {f'{newline}'.join(items)}
+            </items>
         </order>
-        <sender>
-            <company>Express Cargo</company>
-            <phone>+998 98 363 37 67</phone>
-            <town>814303</town>
-            <address>Arnasay 7A</address>
-        </sender>
-        <receiver>
-            <person>{customer_full_name}</person>
-            <phone>{order_instance.phone_number}</phone>
-            <town>{order_instance.town}</town>
-            <address>{order_instance.address}</address>
-        </receiver>
-        <service>{order_instance.service}</service>
-        <items>
-            {''.join(f'<item length="0" height="0" width="0" quantity="1" mass="0" retprice="0" barcode="{item.barcode}">Посылка Express cargo</item>' for item in order_instance.load.products.all())}
-        </items>
     </neworder>
     """
     # <item length="0" height="0" width="0" quantity="1" mass="0" retprice="0" barcode="{}">Посылка Express cargo</item>
