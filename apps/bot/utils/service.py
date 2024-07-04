13fe3f0766368fbe3f1c29cd02dd59d4bb358883
Umarov Kabir <kabirumarov@gmail.com>
2024-07-04 12:16:11 +0500

    changes

--- apps/bot/utils/service.py
+++ apps/bot/utils/service.py
@@ -13,10 +13,13 @@ def language_handler(message: types.Message, bot: TeleBot, web_app_link: str) ->
 
 def send_uz_instruction(message: types.Message, bot: TeleBot, web_app_link: str) -> None:
     file_id = 'BAACAgIAAxkBAAIFy2aGQZH9ijbWsV2bL3d30fui-sDAAAISSAACuzkwSAebLL3Tv_yzNQQ'
+    file_url = 'https://backend.gogocargo.uz/media/instructions/instruction_uz.MP4'
     # file_path = join_path(settings.MEDIA_ROOT, 'instructions', 'instruction_uz.MP4')
     # with open(file_path, 'rb') as video:
-    bot.send_video(chat_id=message.chat.id, video=file_id, supports_streaming=True,
-                   reply_markup=reg_link_web_app_keyboard(web_app_link, 'uz'))
+    # bot.send_video(chat_id=message.chat.id, video=file_id, supports_streaming=True,
+    #                reply_markup=reg_link_web_app_keyboard(web_app_link, 'uz'))
+    bot.send_document(chat_id=message.chat.id, document=file_url,
+                      reply_markup=reg_link_web_app_keyboard(web_app_link, 'uz'))
     remover = bot.send_message(chat_id=message.chat.id, text='.', reply_markup=types.ReplyKeyboardRemove())
     bot.delete_message(chat_id=message.chat.id, message_id=remover.message_id)
 
