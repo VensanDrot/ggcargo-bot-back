515f81ca395ffa35e2c175e7ab97b2d1bf32a9b1
Umarov Kabir <kabirumarov@gmail.com>
2024-07-04 15:14:36 +0500

    telegram send file

--- apps/bot/utils/service.py
+++ apps/bot/utils/service.py
@@ -12,24 +12,19 @@ def language_handler(message: types.Message, bot: TeleBot, web_app_link: str) ->
 
 
 def send_uz_instruction(message: types.Message, bot: TeleBot, web_app_link: str) -> None:
-    file_id = 'BAACAgIAAxkBAAIFy2aGQZH9ijbWsV2bL3d30fui-sDAAAISSAACuzkwSAebLL3Tv_yzNQQ'
-    file_url = 'https://backend.gogocargo.uz/media/instructions/instruction_uz.MP4'
-    # file_path = join_path(settings.MEDIA_ROOT, 'instructions', 'instruction_uz.MP4')
-    # with open(file_path, 'rb') as video:
-    # bot.send_video(chat_id=message.chat.id, video=file_id, supports_streaming=True,
-    #                reply_markup=reg_link_web_app_keyboard(web_app_link, 'uz'))
-    bot.send_document(chat_id=message.chat.id, document=file_url,
-                      reply_markup=reg_link_web_app_keyboard(web_app_link, 'uz'))
+    file_path = join_path(settings.MEDIA_ROOT, 'instructions', 'instruction_uz.mp4')
+    with open(file_path, 'rb') as video:
+        bot.send_video(chat_id=message.chat.id, video=video, supports_streaming=True,
+                       reply_markup=reg_link_web_app_keyboard(web_app_link, 'uz'))
     remover = bot.send_message(chat_id=message.chat.id, text='.', reply_markup=types.ReplyKeyboardRemove())
     bot.delete_message(chat_id=message.chat.id, message_id=remover.message_id)
 
 
 def send_ru_instruction(message: types.Message, bot: TeleBot, web_app_link: str) -> None:
-    file_id = 'BAACAgIAAxkBAAIFzGaGQdG8NUdHrxOizmhpOa6c-Kp1AAIUSAACuzkwSBMp2bWO3gdUNQQ'
-    # file_path = join_path(settings.MEDIA_ROOT, 'instructions', 'instruction_ru.MP4')
-    # with open(file_path, 'rb') as video:
-    bot.send_video(chat_id=message.chat.id, video=file_id, supports_streaming=True,
-                   reply_markup=reg_link_web_app_keyboard(web_app_link, 'ru'))
+    file_path = join_path(settings.MEDIA_ROOT, 'instructions', 'instruction_ru.mp4')
+    with open(file_path, 'rb') as video:
+        bot.send_video(chat_id=message.chat.id, video=video, supports_streaming=True,
+                       reply_markup=reg_link_web_app_keyboard(web_app_link, 'ru'))
     remover = bot.send_message(chat_id=message.chat.id, text='.', reply_markup=types.ReplyKeyboardRemove())
     bot.delete_message(chat_id=message.chat.id, message_id=remover.message_id)
 
